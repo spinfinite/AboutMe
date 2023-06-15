ed759c82330f4449c6f9dec61e247962854e1320
Sergio <spinfinite07@gmail.com>
2023-06-15 12:55:50 -0600

    Initial Commit

---
+++ AboutMeBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeBasic
+//
+//  Created by Dad's 6,1 on 6/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
